eca83cd1a5f31d30f4999d86309fe103c3c82c84
William Kent <wjk@sunsol.me>
2016-04-03 17:32:32 -0400

    Initial commit

---
+++ IKVM.GlobalAssemblyInfo/GlobalAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("William Kent")]
+[assembly: AssemblyProduct("IKVM")]
+[assembly: AssemblyCopyright("Copyright © 2016 William Kent. All rights reserved.")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
