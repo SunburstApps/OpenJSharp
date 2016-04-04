8345047312a83a11e58d3b5ad7999d47af873906
William Kent <wjk@sunsol.me>
2016-04-03 23:21:20 -0400

    Remove additional file during clean

--- IKVM.OpenJDK/psakeProject.ps1
+++ IKVM.OpenJDK/psakeProject.ps1
@@ -191,6 +191,7 @@ Task GenerateSourceList -RequiredVariables IntDir, ProjectDir {
 Task clean -RequiredVariables IntDir, OutDir {
     # Don't delete openjdk-8u45.zip or its expanded contents, due to the size of the download and the time required to unpack it.
 	Remove-Item -Force "$($IntDir)\allsources.gen.lst"
+	Remove-Item -Force "$($IntDir)\classfiles.done"
 	Remove-Item -Force "$($IntDir)\*.jar"
 	Remove-Item -Force "$($OutDir)\vfs.zip"
 
