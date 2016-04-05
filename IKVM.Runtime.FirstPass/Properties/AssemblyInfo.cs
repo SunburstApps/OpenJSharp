6e05969af22bbe7e1f3fa5f8eeeda3c32c9a9564
William Kent <wjk@sunsol.me>
2016-04-04 21:34:26 -0400

    Add missing InternalsVisibleTo attributes

--- IKVM.Runtime.FirstPass/Properties/AssemblyInfo.cs
+++ IKVM.Runtime.FirstPass/Properties/AssemblyInfo.cs
@@ -1,3 +1,13 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("IKVM.Runtime (first pass)")]
+[assembly: InternalsVisibleTo("IKVM.Runtime.JNI")]
+[assembly: InternalsVisibleTo("IKVM.OpenJDK.Core")]
+[assembly: InternalsVisibleTo("IKVM.OpenJDK.Util")]
+[assembly: InternalsVisibleTo("IKVM.OpenJDK.Security")]
+[assembly: InternalsVisibleTo("IKVM.OpenJDK.Management")]
+[assembly: InternalsVisibleTo("IKVM.OpenJDK.Media")]
+[assembly: InternalsVisibleTo("IKVM.OpenJDK.Misc")]
+[assembly: InternalsVisibleTo("IKVM.OpenJDK.Remoting")]
+[assembly: InternalsVisibleTo("IKVM.OpenJDK.SwingAWT")]
