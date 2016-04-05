9aea766ad2e9339bd3a056eb62bac22bdbf54fd0
William Kent <wjk@sunsol.me>
2016-04-04 22:02:39 -0400

    Add IKVM.Awt.WindowsForms assembly

---
+++ IKVM.Awt.WindowsForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Security;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("IKVM.Awt.WindowsForms")]
+[assembly: InternalsVisibleTo("IKVM.OpenJDK.SwingAWT")]
+[assembly: AllowPartiallyTrustedCallers]
