b8f62aa57c74c6683ce05915b6fab1c2a6af2f13
William Kent <wjk@users.noreply.github.com>
2018-01-04 11:52:45 -0500

    Add ReplaceToken task

---
+++ tools/JSharp.BuildTasks.Private/ReplaceToken.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace JSharp.BuildTasks.Private
+{
+    public sealed class ReplaceToken : Task
+    {
+        [Required]
+        public ITaskItem InputFile { get; set; }
+        [Required]
+        public ITaskItem OutputFile { get; set; }
+        [Required]
+        public string[] Replacements { get; set; }
+
+        public override bool Execute()
+        {
+            string text = File.ReadAllText(InputFile.GetMetadata("FullPath"));
+
+            foreach (string replacement in Replacements)
+            {
+                string[] parts = replacement.Split('=');
+                if (parts.Length != 2)
+                {
+                    Log.LogError("Malformed replacement '{0}': Expected exactly one equals sign", replacement);
+                    continue;
+                }
+
+                text = text.Replace(parts[0], parts[1]);
+            }
+
+            if (!Log.HasLoggedErrors) File.WriteAllText(OutputFile.GetMetadata("FullPath"), text);
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
