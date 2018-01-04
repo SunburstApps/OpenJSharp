36dfbac0d3a9ec14e792f5b67393c1fe1ad3440f
William Kent <wjk@users.noreply.github.com>
2018-01-03 22:24:05 -0500

    Add BootstrapStubGenerator task to JSharp.BuildTasks.Private

---
+++ tools/JSharp.BuildTasks.Private/BootstrapStubGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace JSharp.BuildTasks.Private
+{
+    public sealed class BootstrapStubGenerator : ToolTask
+    {
+        [Required]
+        public string InputAssemblyName { get; set; }
+
+        [Required]
+        public ITaskItem OutputDirectory { get; set; }
+
+        [Required]
+        public string ToolLocation { get; set; }
+
+        protected override string ToolName => "jsharpstubgen";
+        protected override string GenerateFullPathToTool() => ToolLocation;
+
+        protected override string GenerateCommandLineCommands()
+        {
+            List<string> argv = new List<string>();
+            argv.Add("-bootstrap");
+            argv.Add(InputAssemblyName);
+            return string.Join(" ", argv.Select(x => $"\"{x}\""));
+        }
+
+        protected override string GetWorkingDirectory()
+        {
+            return OutputDirectory.GetMetadata("FullPath");
+        }
+    }
+}
