d454638a772471b985a5b9761c65e0235a8b8c66
William Kent <wjk@users.noreply.github.com>
2018-01-03 22:29:22 -0500

    Add CreateZipFile task

---
+++ tools/JSharp.BuildTasks.Private/CreateZipFile.cs
@@ -0,0 +1,32 @@
+﻿using System.IO.Compression;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace JSharp.BuildTasks.Private
+{
+    public sealed class CreateZipFile : Task
+    {
+        [Required]
+        public ITaskItem OutputZipFile { get; set; }
+
+        [Required]
+        public ITaskItem[] Contents { get; set; }
+
+        public override bool Execute()
+        {
+            string outputPath = OutputZipFile.GetMetadata("FullPath");
+            using (ZipArchive archive = ZipFile.Open(outputPath, ZipArchiveMode.Create))
+            {
+                foreach (ITaskItem item in Contents)
+                {
+                    string logicalName = item.GetMetadata("NameInZip");
+                    if (string.IsNullOrEmpty(logicalName)) logicalName = item.ItemSpec;
+
+                    archive.CreateEntryFromFile(item.GetMetadata("FullPath"), logicalName);
+                }
+            }
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
