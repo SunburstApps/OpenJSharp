870262b4d38d51b6f37e9c18530d349f97ee5747
William Kent <wjk@users.noreply.github.com>
2018-01-04 22:12:21 -0500

    Add JSharpStubGenerator task

---
+++ tools/JSharp.BuildTasks/JSharpStubGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace JSharp.BuildTasks
+{
+    public sealed class JSharpStubGenerator : ToolTask
+    {
+        [Required]
+        public ITaskItem ToolLocation { get; set; }
+        [Required]
+        public ITaskItem JarFile { get; set; }
+        [Required]
+        public ITaskItem[] InputAssemblies { get; set; }
+        public string[] Namespaces { get; set; }
+
+        protected override string ToolName => "jsharpstubgen";
+        protected override string GenerateFullPathToTool() => ToolLocation.GetMetadata("FullPath");
+
+        protected override string GenerateCommandLineCommands()
+        {
+            List<string> argv = new List<string>();
+
+            argv.Add("-out:" + JarFile.GetMetadata("FullPath"));
+            argv.AddRange(Namespaces.Select(x => "-namespace:" + x));
+            argv.AddRange(InputAssemblies.Select(x => x.GetMetadata("FullPath")));
+
+            return string.Join(" ", argv.Select(x => $"\"{x}\""));
+        }
+    }
+}
